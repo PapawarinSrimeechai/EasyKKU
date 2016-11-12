b52465187a158f5f4e9ea645b434bfc021accb49
papawarinsrimeehchai@gmail.com <papawarinsrimeechai@gmail.com>
2016-11-12 12:09:52 -0800

    sing up

--- app/src/main/java/com/example/miw/easykku/SignUpActivity.java
+++ app/src/main/java/com/example/miw/easykku/SignUpActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SignUpActivity extends AppCompatActivity {
-
+    //signup
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
