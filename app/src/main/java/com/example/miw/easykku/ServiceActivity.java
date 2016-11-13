b7a305c99ce8d3b399464fe88ca522545caeb83d
papawarinsrimeehchai@gmail.com <papawarinsrimeechai@gmail.com>
2016-11-13 14:59:53 -0800

    Call friend

--- app/src/main/java/com/example/miw/easykku/ServiceActivity.java
+++ app/src/main/java/com/example/miw/easykku/ServiceActivity.java
@@ -1,14 +1,23 @@
 package com.example.miw.easykku;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 public class ServiceActivity extends AppCompatActivity {
 
     //Explicit
     private ListView listView;
-    private String[] nameStrings,phoneStrings,imageStrings;
+    private String[] nameStrings, phoneStrings, imageStrings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +33,49 @@ public class ServiceActivity extends AppCompatActivity {
         imageStrings = getIntent().getStringArrayExtra("Image");
 
         //create ListView
-        myAdapter myAdapter = new myAdapter(ServiceActivity.this,nameStrings,phoneStrings,imageStrings);
+        myAdapter myAdapter = new myAdapter(ServiceActivity.this, nameStrings, phoneStrings, imageStrings);
         listView.setAdapter(myAdapter);
 
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long j) {
+                confirmCall(nameStrings[i], phoneStrings[i]);
+            }
+        });
     }//Main method
+
+    private void confirmCall(String nameString, final String phoneString) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ServiceActivity.this);
+        builder.setCancelable(false);
+        builder.setIcon(R.drawable.doremon48);
+        builder.setTitle("Please Confirm");
+        builder.setMessage("คุณต้องการโทรหา " + nameString + " ? ");
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        }); // Cancel
+        builder.setPositiveButton("Call", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Intent intent = new Intent(Intent.ACTION_CALL);
+                intent.setData(Uri.parse("tel:=" + phoneString));
+                if (ActivityCompat.checkSelfPermission(ServiceActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(intent);
+            }
+        }); // Call
+        builder.show();
+    }//confirm
 
 }//Main Class
